76d8cf0e01027af0fa6227f6ee6d29b4b5919b6a
juec0616 <juec0616@gmail.com>
2020-06-24 19:21:40 +0900

    노드 추가 및 노드 출력

--- 소스.cpp
+++ 소스.cpp
@@ -14,8 +14,51 @@ typedef struct _Head
 	int size;
 } Head;
 
+bool nodeInsert(Head* &head, int data)
+{
+	try
+	{
+		Node *tmp = new Node;
+		tmp->data = data;
+		tmp->next = head->head;
+		head->head = tmp;
+		head->size++;
+		return true;
+	}
+	catch (const exception &e)
+	{
+		return false;
+	}
+}
+
+void deleteNode(Head *&head)
+{
+	Node *curr = head->head;
+	while (curr != NULL)
+	{
+		Node *tmp = curr;
+		curr = curr->next;
+		delete tmp;
+	}
+	delete head;
+
+}
+
 int main()
 {
+	Head *head = new Head;
+	head->head = NULL;
+	head->size = 0;
+
+	nodeInsert(head, 10);
+	nodeInsert(head, 20);
+	nodeInsert(head, 30);
+
+	cout << head->head->data << "\n";
+	cout << head->head->next->data << "\n";
+	cout << head->head->next->next->data << "\n";
+
+	deleteNode(head);
 
 	return 0;
 }
